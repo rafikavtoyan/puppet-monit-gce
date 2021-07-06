8efb9de7c5631279a4fec274a27f9095be8ae212
Rafik Avtoyan <rafikavtoyan@gmail.com>
2021-07-06 17:57:18 +0400

    Update config.pp
    delete fs section

--- manifests/config.pp
+++ manifests/config.pp
@@ -40,44 +40,7 @@ class monit::config {
   #  content => template('monit/conf_file_overrides.erb'),
   #}
 
-  # System resources check.
-  $ensure_options = [ present, absent ]
-  if ! ($monit::system_check_ensure in $ensure_options) {
-    fail("Invalid ensure parameter. Valid values: ${ensure_options}")
-  }
-  if $monit::system_check_ensure == present {
-    # Validate number?
-    #  $monit::system_loadavg_1min,
-    #  $monit::system_loadavg_5min,
-    #  $monit::system_loadavg_15min,
-    validate_string(
-      $monit::system_memory,
-      $monit::system_swap,
-      $monit::system_cpu_user,
-      $monit::system_cpu_system,
-      $monit::system_cpu_wait,
-      $monit::system_swap,
-      $monit::system_fs_space_usage,
-      $monit::system_fs_inode_usage,
-    )
-    validate_array($monit::system_fs)
 
-#    monit::check::system {$::fqdn:
-#      priority => '10',
-#      group    => 'system',
-#      order    => 0,
-#      tests    => parseyaml(template('monit/system_test_resources.erb')),
-#    }
-
-    $system_test_fs_defaults = {
-      priority => '10',
-      group    => 'system',
-      bundle   => $::fqdn,
-      order    => 1,
-    }
-    $system_test_fs = parseyaml(template('monit/system_test_filesystems.erb'))
-    create_resources('monit::check::filesystem', $system_test_fs, $system_test_fs_defaults)
-  }
 
   # Additional checks.
   if ($monit::hiera_merge_strategy == 'hiera_hash') {
